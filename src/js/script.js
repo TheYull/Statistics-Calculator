a17f7d16f25fc8c24db45a6223271522834b7295
Yuliia <the.smile.too@gmail.com>
2024-04-07 22:51:25 +0300

    update numbers, adding a filter

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,8 @@
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
-  const numbers = array.map();
+  const numbers = array.map((el) => {
+    Number(el);
+  });
+  const filtered = numbers.filter((el) => !isNaN(el));
 };
