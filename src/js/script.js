5c2ca6346ab8c447efc1548a298e0322b007d0c1
Yuliia <the.smile.too@gmail.com>
2024-04-13 17:17:32 +0300

    add getMedian

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,17 @@
 const getMean = (array) =>
   array.reduce((acc, el) => acc + el, 0) / array.length;
 
+const getMedian = (array) => {
+  const sorted = array.sort((a, b) => a - b);
+  const median =
+    array.length % 2 === 0
+      ? getMean([sorted[array.length / 2], sorted[array.length / 2 - 1]])
+      : sorted[Math.floor(array.length / 2)];
+  return median;
+};
+
+const getMode = (array) => {};
+
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
